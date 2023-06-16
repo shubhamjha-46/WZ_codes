14dae2177aaa54537251d7de62fd379c97ff37f8
Shubham Jha <67794546+shubhamjha-46@users.noreply.github.com>
2023-06-16 18:43:12 +0530

    Add files via upload

--- b_rdaq.py
+++ b_rdaq.py
@@ -1,28 +1,10 @@
-
 import numpy as np
 from numpy import linalg as LA
 import matplotlib
 import matplotlib.pyplot as plt
 import sympy as sp
 
-
-
 np.random.seed(10)
-
-d=512
-n=10
-I=10
-B = 1 #boosting parameter
-print('Clients: {}, dimension: {}, Iteration: {}'.format(n,d, I))
-e=0
-M=[0]*4  ## 4 is enough to cover unity norms
-for i in range(4):
-    M[i]=min(1,np.sqrt(3/d*np.exp(e)))    
-    e=np.exp(e)
-M=[0.0]+M    
-print('Communication per dim: B-RDAQ =',int(4*np.log2(B+1))+2)  # as per the paper; 2 counts for log(len(M))
-
-
 def B_RDAQ(flat_grad, side_inf, diagonal, Bnd):
     #Random Rotation    
     flat_grad=np.multiply(diagonal, flat_grad)        
@@ -30,10 +12,6 @@ def B_RDAQ(flat_grad, side_inf, diagonal, Bnd):
     rotated_flat_grad=sp.fwht(flat_grad)
     rotated_flat_grad = np.array(rotated_flat_grad, dtype=np.float64)
     rotated_norm_grad=rotated_flat_grad/d**0.5
-    #grad_norm=LA.norm(rotated_norm_grad)
-    
-    
-    #Normalizingthegradient
     rotated_norm_grad=rotated_norm_grad/Bnd
     
     # Rotate and normalize the side-information
@@ -46,9 +24,9 @@ def B_RDAQ(flat_grad, side_inf, diagonal, Bnd):
   
 	
     #Nonuniform_Level
-    temp_M=np.digitize(np.absolute(rotated_norm_grad), M)
+    temp_M_1=np.digitize(np.absolute(rotated_norm_grad), M)
     temp_M_2=np.digitize(np.absolute(rotated_norm_side), M)
-    temp_M=np.maximum(temp_M, temp_M_2)
+    temp_M=np.maximum(temp_M_1, temp_M_2)
    
     temp_2=[0.0]*d
     temp_2_s=[0.0]*d
@@ -59,14 +37,13 @@ def B_RDAQ(flat_grad, side_inf, diagonal, Bnd):
    
     output=np.array([0.0]*d)
     for w in range(B):
-        U_random = np.random.rand(d)
+        U_random = np.random.rand(d)   #### Indeoendent shared randomness per repetition
         b_temp_2=UnifQ(temp_2, U_random, temp_M)         # Use of indicators to find unbiased estimates
         b_temp_2_s=UnifQ(temp_2_s, U_random, temp_M)
     #print(temp_2, temp_2_s)
         otp=[0.0]*d
         cd=[0.0]*d
         for i in range(d):
-        
             otp[i]=(b_temp_2[i]-b_temp_2_s[i])*2*M[temp_M[i]]
             cd[i] = b_temp_2[i]-b_temp_2_s[i]
             otp[i]+=rotated_norm_side[i]
@@ -79,73 +56,79 @@ def B_RDAQ(flat_grad, side_inf, diagonal, Bnd):
     output = list(output)
     output_v=np.array(sp.ifwht(output), dtype=np.float64)
     output_v=np.multiply(output_v, diagonal)
-
     return  output_v
     
 
 
-    
-    
 def UnifQ(input_v, U_r, tmp):
-    U    = [2*M[tmp[i]]*(U_r[i]-0.5) for i in range(d)]    #### generates uniform random variable in [-M,M]
+    U = [2*M[tmp[i]]*(U_r[i]-0.5) for i in range(d)]    #### generates uniform random variable in [-M,M]
     #print(U)
     bits = np.array([np.where(U[i]<=input_v[i], 1.,0.) for i in range(d)])
     return bits
 
-''' Note: In the following, we take sigma_md as our tuning parameter. Its is realted as delta_prime/8 to our paper description. '''
+for n in [5,10,20,30, 40, 50]:
+	d = 512
+	I = 20
+	B = 1 #boosting parameter
+	''' Note: In the following, we take sigma_md as our tuning parameter. Its is realted as delta_prime/8 to our paper description. '''
 
-sigma_range=[0.000078125*(2**i) for i in range(8)]  ### Uncomment it for 6 and 8  bit comparisons
-#sigma_range=[0.000078125*(2**(i-2)) for i in range(8)]  ## uncomment it only for 10 bit comparisons
+	sigma_range = [0.0125/16]  ### Only for comparison varying clients
+	#sigma_range = [0.000078125*(2**(i-4)) for i in range(8)]  ### Uncomment it for 6 and 8  bit comparisons
+	#sigma_range=[0.000078125*(2**(i-4)) for i in range(8)]  ## uncomment it only for 10 bit comparisons
 
-MSE_B_RDAQ=[]
-for sig in sigma_range:
-    sigma_md= sig
-    Bnd = (1+4*sig)*np.sqrt(d)
-    MSE_B_RDAQ_I=[0.0]*I
-    for j in range(I):
-        frac=int(d**((j-1)/10.0))
-        input_avg=[0.0]*d
-        output_avg=[0.0]*d 
-        output_avg_ratq=[0.0]*d
-        output_avg_rmq=[0.0]*d
-        norm_ip=0.0
-        mean = np.random.rand(d)
-        for client in range(n):
-             diagonal=2*np.random.binomial(1, 0.5, d)-[1.0]*d    #### independent rotation  for each client
-             
-             t = 8*sigma_md*(np.random.rand(d)-0.5)    #### independent data for each client though closer by metric: \sigma_md
-             x= mean+t
-             
-             y = mean+8*sigma_md*(np.random.rand(d)-0.5) 
-             res = max(abs(y-x))
-            
-             
-             Output_V=B_RDAQ(x, y, diagonal, Bnd)           
-             input_avg=np.add(x, input_avg)
-             
-             output_avg=np.add(Output_V, output_avg)
-             print('Iteration:{}, client: {}, sigma_MD: {}'.format(j, client, sigma_md))       
-        MSE1=np.add(output_avg/n, -input_avg/n)
-        print('Norm: TM=', LA.norm(np.array(input_avg))/n, 'B_RDAQ= ', LA.norm(np.array(output_avg))/n)
-        MSE1=np.array(MSE1, dtype=np.float64)
-        MSE_B_RDAQ_I[j]=LA.norm(MSE1)
-        
-       
-   
-    print('Average RMSE over iterations: RDAQ', np.mean(MSE_B_RDAQ_I))
-    MSE_B_RDAQ.append(np.mean(MSE_B_RDAQ_I)) 
- 
-font = {'family': 'normal', 'weight': 'normal', 'size': 14}
-matplotlib.rc('font', **font)
+	print('Clients: {}, dimension: {}, Iteration: {}'.format(n,d, I))
+	e = 0
+	M = [0]*4  ## 4 is enough to cover unity norms
+	for i in range(4):
+		M[i] = min(1,np.sqrt(3/d*np.exp(e)))    
+		e = np.exp(e)
+	M = [0.0]+M    
+	print('Communication per dim: B-RDAQ =',int(4*np.log2(B+1))+2)  # as per the paper; 2 counts for log(len(M))
+	MSE_B_RDAQ = []
+	for sig in sigma_range:
+		sigma_md = sig
+		Bnd = (1+4*sig)*np.sqrt(d)
+		MSE_B_RDAQ_I = [0.0]*I
+		for j in range(I):
+			input_avg = [0.0]*d
+			output_avg = [0.0]*d
+			mean = np.random.rand(d)
+			for client in range(n):
+				 diagonal = 2*np.random.binomial(1, 0.5, d)-[1.0]*d    #### independent rotation  for each client
+				 
+				 t = 8*sigma_md*(np.random.rand(d)-0.5)    #### independent data for each client though closer by metric: \sigma_md
+				 x = mean+t
+				 
+				 y = mean + 8*sigma_md*(np.random.rand(d)-0.5) 
+				 res = max(abs(y-x))
+				
+				 
+				 Output_V = B_RDAQ(x, y, diagonal, Bnd)           
+				 input_avg = np.add(x, input_avg)
+				 
+				 output_avg = np.add(Output_V, output_avg)
+				 print('Iteration:{}, client: {}, sigma_MD: {}'.format(j, client, sigma_md))       
+			MSE1 = np.add(output_avg/n, -input_avg/n)
+			print('Norm: TM =', LA.norm(np.array(input_avg))/n, 'B_RDAQ = ', LA.norm(np.array(output_avg))/n)
+			MSE1 = np.array(MSE1, dtype=np.float64)
+			MSE_B_RDAQ_I[j] = LA.norm(MSE1)
+			
+		   
+		print('RMSE over iterations: RDAQ', MSE_B_RDAQ_I)
+		print('Average RMSE over iterations: RDAQ', np.mean(MSE_B_RDAQ_I))
+		MSE_B_RDAQ.append(np.mean(MSE_B_RDAQ_I))
+	 
+	font = {'family': 'normal', 'weight': 'normal', 'size': 14}
+	matplotlib.rc('font', **font)
 
 
-np.savetxt("RMSE_B_RDAQ_"+str(int(4*np.log2(B+1))+2)+"_"+str(d)+".dat", MSE_B_RDAQ, delimiter =", ", fmt ='% s')
+	np.savetxt('n='+str(n)+"_RMSE_B_RDAQ_"+str(int(4*np.log2(B+1))+2)+"_"+str(d)+".dat", MSE_B_RDAQ, delimiter =", ", fmt ='% s')
 
-plt.plot(sigma_range, MSE_B_RDAQ, label='B_RDAQ '+str(int(4*np.log2(B+1))+2)+'-bits')
+	plt.plot(sigma_range, MSE_B_RDAQ, label='B_RDAQ '+str(int(4*np.log2(B+1))+2)+'-bits')
 plt.legend()
 plt.xlabel('sigma_MD')
 plt.ylabel('RMSE')
 plt.xticks(ticks= sigma_range, labels=sigma_range)
 plt.title('n='+str(n)+' d='+str(d)+' iter='+str(I))
 plt.grid()
-plt.show() 
+plt.show()
--- rmq.py
+++ rmq.py
@@ -3,27 +3,10 @@ from numpy import linalg as LA
 import matplotlib
 import matplotlib.pyplot as plt
 import sympy as sp
+import math
 
 
 np.random.seed(10) 
-
-d=1024 # dimensions
-n=10 # clients
-I=10 # iterations
-print('Clients: {}, dimension: {}, Iteration: {}'.format(n,d, I))
-e=0
-M=[0]*4   # tetra-iteration levels
-for i in range(4):
-    M[i]=min(1,np.sqrt(3/d*np.exp(e)))    
-    e=np.exp(e)
-
-M=[0.0]+M    
-K=10            #No. of bits used for RMQ
-
-
-print('Communication per dim: RMQ =', K)  
-### Unif_K=[-1., 1.]
-
 
 def RMQ(x,y, diagonal, U_rand, res):
 	eps = 4*res/(2**(K)-2)  # as per the paper
@@ -44,32 +27,40 @@ def RMQ(x,y, diagonal, U_rand, res):
 	z = rotated_norm_x/eps
 	z=list(z)
 	z_l=[int(z[i]) for i in range(d)]
-	bt=[np.where(U_rand[i]<= z[i]+z_l[i], z_l[i]+1, z_l[i]) for i in range(d)]  
+	bt=[np.where(U_rand[i]<= z[i]-z_l[i], z_l[i]+1, z_l[i]) for i in range(d)]  
 	bt_encoded =[int(i)%(2**(K)) for i in bt]
 	
 	## Decoder uses only y, K, eps, bt_encoded
 	output=[0.0]*d
 	W=[]
 	for i in range(d):
-		dist1=1
-		dist2=0
-		
-		w=4500  ### Set 1500 for 6 bits; 4500 for 10 bits. Though, can be optimized.
+		m1 = math.floor((rotated_norm_y[i]/eps+2**K-bt_encoded[i])/2**K)
+		m2 = math.floor((rotated_norm_y[i]/eps-2**K-bt_encoded[i])/2**K)
+		#print(m1,m2)
+		t_var = m2
+		chc = []
+		while t_var<=m1:
+			chc.append(np.abs((t_var*(2**K)+bt_encoded[i])*eps-rotated_norm_y[i]))
+			t_var+=1
+		w	=	np.argmin(chc)+m2
+		output[i] = (bt_encoded[i]+w*2**(K))*eps
+		#### Brute-force method #####
 		'''For smaller delta, search space needs to be large to find argmin in RMQ decoder. 
 	    One way to see- smaller delta->small epsilon->more number of K-size windows. 
 		Search space should be proportional to multiplicative increment in delta.'''
+		
+		'''dist1=1
+		dist2=0
+		w=4500  ### Set 1500 for 6 bits; 4500 for 10 bits. Though, can be optimized.
 		while dist1>=dist2:
 			w-=1
-			#print(eps)
-			#print((rotated_norm_y[i]-2**(K)*eps), (rotated_norm_y[i]+2**(K)*eps))
 			dist1 = np.abs((bt_encoded[i]+w*2**(K))*eps-rotated_norm_y[i])
 			dist2 = np.abs((bt_encoded[i]+(w-1)*2**(K))*eps-rotated_norm_y[i])
 		W.append(w)
-		output[i] = (bt_encoded[i]+w*2**(K))*eps
-		
-	# Inverse rotation
-	print('W=',max(W))
+		#if (np.argmin(chc)+m2==w):
+		#	print('true')'''
 
+	####Inverse rotation######
 	output = np.array(output)*(d**0.5)
 	output = list(output)
 	output_v=np.array(sp.ifwht(output), dtype=np.float64)
@@ -77,58 +68,75 @@ def RMQ(x,y, diagonal, U_rand, res):
 	return  output_v
 
 
-''' Note: In the following, we take sigma_md as our tuning parameter. Its is realted as delta_prime/8 to our paper description. '''
+''' 	1. Note: In the following, we take sigma_md as our tuning parameter. Its is realted as delta_prime/8 to our paper description. 
+	2. We are using different values of x axis for 6 and 10 bits.'''
+	
+d = 512 # dimensions
+I = 20 # iterations
+e = 0
+M = [0]*4   # tetra-iteration levels
+for i in range(4):
+    M[i] = min(1,np.sqrt(3/d*np.exp(e)))    
+    e = np.exp(e)
 
-#sigma_range=[0.000078125*(2**i) for i in range(1)]  ### Uncomment it for 6 and 8  bit comparisons
-sigma_range=[0.000078125*(2**(i-2)) for i in range(1)]  ## uncomment it only for 10 bit comparisons
+M = [0.0]+M    
+K = 6          #No. of bits used for RMQ
+print('Communication per dim: RMQ =', K)  
 
-MSE_RMQ=[]
-for sig in sigma_range:
-    sigma_md= sig
-    res = 8*sig
-    MSE_RMQ_I=[0.0]*I
-    for j in range(I):
-        frac=int(d**((j-1)/10.0))
-        input_avg=[0.0]*d
-        output_avg_rmq=[0.0]*d
-        norm_ip=0.0
-        mean = np.random.rand(d)
-        for client in range(n):
-             diagonal=2*np.random.binomial(1, 0.5, d)-[1.0]*d    #### independent rotation  for each client
-             
-             t = 8*sigma_md*(np.random.rand(d)-0.5)   
-             x= mean+t  # sampling the input
-             y = mean+8*sigma_md*(np.random.rand(d)-0.5) # side-information
-             
-             #res = max(abs(y-x))   # required for choosing parameter epsilon in RMQ
-             
-             U_random = np.random.rand(d)
-            
-             Output_V_Rmq=RMQ(x,y, diagonal, U_random, res)
-            
-             input_avg=np.add(x, input_avg)
-            
-             output_avg_rmq=np.add(Output_V_Rmq, output_avg_rmq)
-             print('Iteration:{}, client: {}, sigma_MD: {}'.format(j, client, sigma_md))       
-        
-        MSE3=np.add(output_avg_rmq/n, -input_avg/n)
-        print('Norm: TM=', LA.norm(np.array(input_avg))/n, 'RMQ= ', LA.norm(np.array(output_avg_rmq))/n)
-        MSE3=np.array(MSE3, dtype=np.float64)
-        MSE_RMQ_I[j]=LA.norm(MSE3)
 
-    print('Average RMSE over iterations: RMQ', np.mean(MSE_RMQ_I))
-    MSE_RMQ.append(np.mean(MSE_RMQ_I)) 
+for n in [5,10,20,30,40,50]:
+	print('Clients: {}, dimension: {}, Iteration: {}'.format(n,d, I))
+	sigma_range = [0.0125/16]  ### Only for comparison varying clients
+	#sigma_range = [0.000078125*(2**(i-2)) for i in range(8)]  ### Uncomment it for 6 and 8  bit comparisons
+	#sigma_range=[0.000078125*(2**(i-4)) for i in range(8)]  ## uncomment it only for 10 bit comparisons
 
-font = {'family': 'normal', 'weight': 'normal', 'size': 14}
-matplotlib.rc('font', **font)
+	MSE_RMQ = []
+	for sig in sigma_range:
+		sigma_md = sig
+		res = 8*sig
+		MSE_RMQ_I = [0.0]*I
+		for j in range(I):
+			frac = int(d**((j-1)/10.0))
+			input_avg = [0.0]*d
+			output_avg_rmq = [0.0]*d
+			norm_ip = 0.0
+			mean = np.random.rand(d)
+			for client in range(n):
+				 diagonal = 2*np.random.binomial(1, 0.5, d)-[1.0]*d    #### independent rotation  for each client
+				 
+				 t = 8*sigma_md*(np.random.rand(d)-0.5)   
+				 x= mean+t  # sampling the input
+				 y = mean+8*sigma_md*(np.random.rand(d)-0.5) # side-information                          
+				 U_random = np.random.rand(d)
+				
+				 Output_V_Rmq = RMQ(x,y, diagonal, U_random, res)
+				
+				 input_avg = np.add(x, input_avg)
+				
+				 output_avg_rmq = np.add(Output_V_Rmq, output_avg_rmq)
+				 print('Iteration:{}, client: {}, sigma_MD: {}'.format(j, client, sigma_md))       
+			
+			MSE3 = np.add(output_avg_rmq/n, -input_avg/n)
+			print('Norm: TM=', LA.norm(np.array(input_avg))/n, 'RMQ= ', LA.norm(np.array(output_avg_rmq))/n)
+			MSE3 = np.array(MSE3, dtype=np.float64)
+			MSE_RMQ_I[j] = LA.norm(MSE3)
 
-#np.savetxt("RMSE_RMQ_"+str(K)+"_"+str(d)+".dat", MSE_RMQ, delimiter =", ", fmt ='% s')
-plt.plot(sigma_range, MSE_RMQ, label='RMQ '+str(K)+'-bits')
+		print('Average RMSE over iterations: RMQ', np.mean(MSE_RMQ_I))
+		MSE_RMQ.append(np.mean(MSE_RMQ_I)) 
+
+	font = {'weight': 'normal', 'size': 14}
+	matplotlib.rc('font', **font)
+
+	np.savetxt('n='+str(n)+"_RMSE_RMQ_"+str(K)+"_"+str(d)+".dat", MSE_RMQ, delimiter =", ", fmt ='% s')
+	plt.plot(sigma_range, MSE_RMQ, label='RMQ '+str(K)+'-bits')
 
 plt.legend()
 plt.xlabel('sigma_MD')
 plt.ylabel('RMSE')
-plt.xticks(ticks= sigma_range, labels=sigma_range)
+plt.yscale('log')
+plt.xscale('log')
+plt.yticks(fontsize=7)
+plt.xticks(ticks= sigma_range, labels=sigma_range, fontsize=7)
 plt.title('n='+str(n)+' d='+str(d)+' iter='+str(I))
 plt.grid()
-plt.show() 
+plt.show()
